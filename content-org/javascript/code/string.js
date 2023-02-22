8b1b6c39b07d5f7cdbcdc1e2a7d324ccdd9a2db2
richard <yufengwang1024@gmail.com>
2023-02-22 21:12:40 +0800

    feat: update

--- content-org/javascript/code/string.js
+++ content-org/javascript/code/string.js
@@ -1,5 +1,6 @@
 /**
  * @param {string} str
+ * 字符串首字母大写
  */
 function ucFirst(str) {
   return str.substring(0, 1).toUpperCase() + str.substring(1, str.length);
--- content-org/javascript/code/throttle.js
+++ content-org/javascript/code/throttle.js
@@ -1,3 +1,5 @@
+//节流, 每隔一段时间执行一次
+// 例子： 滚动条滚动使，没必要响应每次滚动事件，每隔500ms响应一次即可
 function throttle(fn, time) {
   let timer;
   return function () {
@@ -11,7 +13,7 @@ function throttle(fn, time) {
     timer = setTimeout(func, time);
   };
 }
-
+// 例子：用户在 input 框输入，一段时间没有输入后，再发起网络请求
 function debounce(fn, time) {
   let timeout;
   return function () {
