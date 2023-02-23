ae226e94f8ed0da07572975f243a878633a167e4
richard <yufengwang1024@gmail.com>
2023-02-23 22:38:31 +0800

    feat: update

--- content-org/framework/comp.js
+++ content-org/framework/comp.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const e = React.createElement;
-const createRoot =ReactDOM.createRoot
+const createRoot = ReactDOM.createRoot
+const render = ReactDOM.render
 
 
 class LikeButton extends React.PureComponent {
@@ -30,42 +31,68 @@ class LikeButton extends React.PureComponent {
   }
 }
 
-// function Counter() {
-//   const [state, setState] = React.useState(1);
-//   return (
-//     <div>
-//       <h1 onClick={() => setState((c) => c + 1)}>
-//         <span>Count:</span>
-//         <b>{state}</b>
-//       </h1>
-//       <h2>hello</h2>
-//     </div>
-//   );
-// }
-
+/*function Counter() {
+  const [state, setState] = React.useState(1);
+  const [data, setData] = React.useState([{ id: 1, name: 'a' }, { id: 2, name: 'b' }])
+  React.useEffect(() => {
+    console.log('state')
+  }, [state])
+  return (
+    <div>
+      <h1 onClick={() => setState((c) => c + 1)}>
+        <span>Count:</span>
+        <b>{state}</b>
+      </h1>
+      <h2>hello</h2>
+      <ul>
+        {data.map((el) => {
+          return <li key={el.id}> {el.name}</li>
+        })}
+      </ul>
+    </div>
+  );
+} */
 
 function Counter() {
   const [state, setState] = React.useState(1);
-  return /*#__PURE__*/ React.createElement(
+  const [data, setData] = React.useState([
+    { id: 1, name: 'a' },
+    { id: 2, name: 'b' },
+  ]);
+  React.useEffect(() => {
+    console.log('state');
+  }, [state]);
+  return React.createElement(
     'div',
     null,
-    /*#__PURE__*/ React.createElement(
+    React.createElement(
       'h1',
-      {
-        onClick: () => setState((c) => c + 1),
-      },
-      /*#__PURE__*/ React.createElement('span', null, 'Count:'),
-      /*#__PURE__*/ React.createElement('b', null, state)
+      { onClick: () => setState((c) => c + 1) },
+      React.createElement('span', null, 'Count:'),
+      React.createElement('b', null, state)
     ),
-    /*#__PURE__*/ React.createElement('h2', null, 'hello')
+    React.createElement('h2', null, 'hello'),
+    React.createElement(
+      'ul',
+      null,
+      data.map((el) => {
+        return React.createElement('li', { key: el.id }, el.name);
+      })
+    )
   );
 }
+
+
+
 const element = /*#__PURE__*/ React.createElement(Counter, null);
 
 // const domContainer = document.querySelector("#root");
 const container = document.getElementById("root");
 // const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(element);
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(element);
+
+render(element, container)
+
 // ReactDOM.hydrate(e(Counter), root);
 // ReactDOM.render(e(Counter), root);
