fbb03f5a4be7b66c3dd4cc8f99d585b9640ff4b3
richard <yufengwang1024@gmail.com>
2023-02-26 11:33:50 +0800

    feat: update

--- content-org/javascript/code/event-loop.js
+++ content-org/javascript/code/event-loop.js
@@ -57,3 +57,118 @@ Promise.resolve()
   .then(() => {
     console.log(6);
   });
+
+// 0
+// 1
+// 4
+// 2
+// 3
+// 5
+// 6
+
+// 问输出顺序
+const p = new Promise((resolve) => {
+  console.log(0);
+  resolve();
+});
+p.then((res) => {
+  console.log(1);
+})
+  .then((res) => {
+    console.log(2);
+  })
+  .then((res) => {
+    console.log(3);
+  });
+p.then((res) => {
+  console.log(4);
+});
+p.then((res) => {
+  console.log(5);
+});
+p.then((res) => {
+  console.log(6);
+});
+
+//问输出顺序
+async function async1() {
+  console.log("async1 start");
+  await async2();
+  console.log("async1 end");
+}
+async function async2() {
+  console.log("async2");
+}
+console.log("script start");
+setTimeout(function () {
+  console.log("setTimeout");
+}, 0);
+async1();
+new Promise(function (resolve) {
+  console.log("promise1");
+  resolve();
+}).then(function () {
+  console.log("promise2");
+});
+console.log("script end");
+
+// 问输出顺序
+async function async1() {
+  console.log("async1 start");
+  await async2();
+  console.log("async1 end");
+}
+async function async2() {
+  console.log("async2 start");
+  return new Promise((resolve, reject) => {
+    reject();
+    console.log("async2 promise");
+  });
+}
+
+console.log("illegalscript start");
+
+setTimeout(function () {
+  console.log("setTimeout");
+}, 0);
+
+async1();
+
+new Promise(function (resolve) {
+  console.log("promise1");
+  resolve();
+})
+  .then(function () {
+    console.log("promise2");
+  })
+  .then(function () {
+    console.log("promise3");
+  });
+console.log("illegalscript end");
+
+// 问输出顺序
+const p = new Promise((resolve, reject) => {
+  console.log(0);
+  reject();
+  console.log(1);
+  resolve();
+  console.log(2);
+});
+p.then((res) => {
+  console.log(3);
+})
+  .then((res) => {
+    console.log(4);
+  })
+  .catch((res) => {
+    console.log(5);
+  })
+  .then((res) => {
+    console.log(6);
+  })
+  .catch((res) => {
+    console.log(7);
+  })
+  .then((res) => {
+    console.log(8);
+  });
